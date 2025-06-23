30b8307e0dae6cb15951594f9fb0e38219453a2e
Mohamed Amine Ratit <104103359+m1amineratit@users.noreply.github.com>
2025-06-23 17:29:04 +0100

    Updated LandingPage.tsx

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -55,7 +55,7 @@ const LandingPage: React.FC = () => {
               <a href="#testimonials" className="text-slate-600 hover:text-blue-600 transition-colors font-medium">Reviews</a>
             </div>
 
-            <div className="flex items-center space-x-5">
+            <div className="flex items-center space-x-4">
               <Link
                 to="/login"
                 className="text-slate-600 hover:text-blue-600 transition-colors font-medium"
@@ -64,7 +64,7 @@ const LandingPage: React.FC = () => {
               </Link>
               <Link
                 to="/signup"
-                className="px-5 py-3.5 bg-gradient-to-r from-blue-500 to-violet-600 text-white rounded-xl hover:from-blue-600 hover:to-violet-700 transition-all transform hover:scale-95 shadow-lg hover:shadow-xl font-medium"
+                className="px-4 py-3.5 bg-gradient-to-r from-blue-500 to-violet-600 text-white rounded-xl hover:from-blue-600 hover:to-violet-700 transition-all transform hover:scale-95 n shadow-lg hover:shadow-xl font-medium"
               >
                 Get Started Free
               </Link>
